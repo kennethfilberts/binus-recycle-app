f5d7bbabc44fd94daec66c021095029e0967b800
kennethfilberts <kennethfilbert686@gmail.com>
2023-03-07 19:03:26 +0700

    commit dl

--- components/splash.tsx
+++ components/splash.tsx
@@ -1,18 +1,24 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import React from "react";
+import {StyleSheet, Text, View, Image} from 'react-native';
+import React from 'react';
 
 export default function Splash() {
   return (
-    <View>
-        <Image source={require('../public/logo.png')} style={styles.logo}></Image>
+    <View style={styles.body}>
+      <Image source={require('../public/logo.png')} style={styles.logo}></Image>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-    logo: {
-        width: 150,
-        height: 150,
-        borderRadius: 75,
-    },
+  body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  logo: {
+    width: 150,
+    height: 150,
+    borderRadius: 75,
+  },
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import Splash from '../components/splash';
 import Login from '../components/login';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
 function App(): JSX.Element {
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.body}>
-      {/* <Splash /> */}
-      {/* <Login /> */}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='splash' component={Splash}/>
+        <Stack.Screen name='login' component={Login}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  body: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    color: 'black',
-    fontSize: 24,
-  },
+  
 });
 
 export default App;
